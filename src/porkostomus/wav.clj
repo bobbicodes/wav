ed4f01d133a24b2f0150c3dc1b7a294281663fde
porkostomus <porkostomus@mail.com>
2020-02-24 00:29:20 -0800

    find WAVE chunk size

--- src/porkostomus/wav.clj
+++ src/porkostomus/wav.clj
@@ -5,10 +5,10 @@
   (with-open [in  (io/input-stream file)
               out (java.io.ByteArrayOutputStream.)]
     (io/copy in out)
-    (map #(format "%02x" %) (.toByteArray out))))
+    (.toByteArray out)))
 
 (defn bytes->hex [bytes]
-  (apply str (for [b bytes] (format "%02x" b))))
+  (for [b bytes] (format "%02x" b)))
 
 (defn file->hex [file]
   (bytes->hex (file->bytes file)))
@@ -17,15 +17,25 @@
   ([file n] (hex-bytes file n (inc n)))
   ([file from to]
    (take (- to from)
+         (drop from (file->hex file)))))
+
+(defn dec-bytes
+  ([file n] (hex-bytes file n (inc n)))
+  ([file from to]
+   (take (- to from)
          (drop from (file->bytes file)))))
 
 (defn riff-id [file]
-  (apply str (map char (hex-bytes file 0 4))))
+  (apply str (map char (dec-bytes file 0 4))))
+
+(defn chunk-size [file]
+  (Integer/decode (str "0x" (apply str (reverse (hex-bytes file 4 8))))))
 
 (comment
   (format "%x" -84)
   (bit-and 0xFF 52)
-  (take 8 (file->bytes "resources/test.wav"))
+  (take 8 (file->hex "resources/test.wav"))
   (riff-id  "resources/test.wav")
+  (chunk-size  "resources/test.wav")
   (hex-bytes "resources/test.wav" 4 8)
   )
