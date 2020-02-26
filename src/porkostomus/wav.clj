7021f4fbb74874109df58d2dcdb7170579936c5e
porkostomus <porkostomus@mail.com>
2020-02-26 02:02:05 -0800

    determine header size

--- src/porkostomus/wav.clj
+++ src/porkostomus/wav.clj
@@ -1,5 +1,6 @@
 (ns porkostomus.wav
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn file->bytes
   [file]
@@ -23,17 +24,52 @@
   (Integer/decode (str "0x" (apply str (reverse (take (- to from)
                                                       (drop from (file->hex file))))))))
 
-(defn riff-chunk
+(defn pcm-144
   [file]
-  {:ckID (ascii-val file 0 4)
-   :cksize (dec-val file 4 8)
-   :WAVEID (ascii-val file 8 12)})
+  {:ckID                (ascii-val file 0 4)
+   :cksize              (dec-val file 4 8)
+   :WAVEID              (ascii-val file 8 12)
+   :fmtID               (ascii-val file 12 16)
+   :fmtsize             (dec-val file 16 20)
+   :wFormatTag          (dec-val file 20 22)
+   :nChannels           (dec-val file 22 24)
+   :nSamplesPerSec      (dec-val file 24 28)
+   :nAvgBytesPerSec     (dec-val file 28 32)
+   :nBlockAlign         (dec-val file 32 34)
+   :wBitsPerSample      (dec-val file 34 36)
+   :cbSize              (dec-val file 36 38)
+   :wValidBitsPerSample (dec-val file 38 40)
+   :dwChannelMask       (dec-val file 40 44)
+   :SubFormat           (apply str (take 16 (drop 44 (file->hex file))))})
 
-(defn fmt-chunk
+(defn pcm-72
   [file]
-  {:ckID (ascii-val file 12 16)})
+  {:ckID            (ascii-val file 0 4)
+   :cksize          (dec-val file 4 8)
+   :WAVEID          (ascii-val file 8 12)
+   :fmtID           (ascii-val file 12 16)
+   :fmtsize         (dec-val file 16 20)
+   :wFormatTag      (dec-val file 20 22)
+   :nChannels       (dec-val file 22 24)
+   :nSamplesPerSec  (dec-val file 24 28)
+   :nAvgBytesPerSec (dec-val file 28 32)
+   :nBlockAlign     (dec-val file 32 34)
+   :wBitsPerSample  (dec-val file 34 36)
+   :dataID          (dec-val file 36 40)
+   :datasize        (dec-val file 40 44)})
+
+(defn header-size
+  [file]
+  (count (first (str/split (apply str (take 100 (file->hex file))) #"64617461"))))
+
+(defn wav-info
+  [file]
+  (case (header-size file)
+    144 (pcm-144 file)
+    72 (pcm-72 file)
+    "Unrecognized file type"))
 
 (comment
-  (riff-chunk "resources/test.wav")
-  (fmt-chunk "resources/test.wav")
+(wav-info "resources/test.wav")
+  (wav-info "resources/saw.wav")
   )
