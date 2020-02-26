fbb3f63789b944462f66153e547738a4d86547e0
porkostomus <porkostomus@mail.com>
2020-02-26 02:27:31 -0800

    refactor for enhanced readability

--- src/porkostomus/wav.clj
+++ src/porkostomus/wav.clj
@@ -14,53 +14,54 @@
   (for [byte (file->bytes file)]
     (format "%02x" byte)))
 
-(defn ascii-val 
+(defn ascii
   [file from to]
-  (apply str (map char (take (- to from)
-                             (drop from (file->bytes file))))))
+  (let [bytes (take (- to from) (drop from (file->bytes file)))]
+    (apply str (map char bytes))))
 
-(defn dec-val 
+(defn decimal 
   [file from to]
-  (Integer/decode (str "0x" (apply str (reverse (take (- to from)
-                                                      (drop from (file->hex file))))))))
+  (let [bytes (take (- to from) (drop from (file->hex file)))]
+    (Integer/decode (str "0x" (apply str (reverse bytes))))))
 
 (defn pcm-144
   [file]
-  {:ckID                (ascii-val file 0 4)
-   :cksize              (dec-val file 4 8)
-   :WAVEID              (ascii-val file 8 12)
-   :fmtID               (ascii-val file 12 16)
-   :fmtsize             (dec-val file 16 20)
-   :wFormatTag          (dec-val file 20 22)
-   :nChannels           (dec-val file 22 24)
-   :nSamplesPerSec      (dec-val file 24 28)
-   :nAvgBytesPerSec     (dec-val file 28 32)
-   :nBlockAlign         (dec-val file 32 34)
-   :wBitsPerSample      (dec-val file 34 36)
-   :cbSize              (dec-val file 36 38)
-   :wValidBitsPerSample (dec-val file 38 40)
-   :dwChannelMask       (dec-val file 40 44)
+  {:ckID                (ascii file 0 4)
+   :cksize              (decimal file 4 8)
+   :WAVEID              (ascii file 8 12)
+   :fmtID               (ascii file 12 16)
+   :fmtsize             (decimal file 16 20)
+   :wFormatTag          (decimal file 20 22)
+   :nChannels           (decimal file 22 24)
+   :nSamplesPerSec      (decimal file 24 28)
+   :nAvgBytesPerSec     (decimal file 28 32)
+   :nBlockAlign         (decimal file 32 34)
+   :wBitsPerSample      (decimal file 34 36)
+   :cbSize              (decimal file 36 38)
+   :wValidBitsPerSample (decimal file 38 40)
+   :dwChannelMask       (decimal file 40 44)
    :SubFormat           (apply str (take 16 (drop 44 (file->hex file))))})
 
 (defn pcm-72
   [file]
-  {:ckID            (ascii-val file 0 4)
-   :cksize          (dec-val file 4 8)
-   :WAVEID          (ascii-val file 8 12)
-   :fmtID           (ascii-val file 12 16)
-   :fmtsize         (dec-val file 16 20)
-   :wFormatTag      (dec-val file 20 22)
-   :nChannels       (dec-val file 22 24)
-   :nSamplesPerSec  (dec-val file 24 28)
-   :nAvgBytesPerSec (dec-val file 28 32)
-   :nBlockAlign     (dec-val file 32 34)
-   :wBitsPerSample  (dec-val file 34 36)
-   :dataID          (dec-val file 36 40)
-   :datasize        (dec-val file 40 44)})
+  {:ckID            (ascii file 0 4)
+   :cksize          (decimal file 4 8)
+   :WAVEID          (ascii file 8 12)
+   :fmtID           (ascii file 12 16)
+   :fmtsize         (decimal file 16 20)
+   :wFormatTag      (decimal file 20 22)
+   :nChannels       (decimal file 22 24)
+   :nSamplesPerSec  (decimal file 24 28)
+   :nAvgBytesPerSec (decimal file 28 32)
+   :nBlockAlign     (decimal file 32 34)
+   :wBitsPerSample  (decimal file 34 36)
+   :dataID          (decimal file 36 40)
+   :datasize        (decimal file 40 44)})
 
 (defn header-size
   [file]
-  (count (first (str/split (apply str (take 100 (file->hex file))) #"64617461"))))
+  (let [s (apply str (take 100 (file->hex file)))]
+    (count (first (str/split s #"64617461")))))
 
 (defn wav-info
   [file]
@@ -70,6 +71,5 @@
     "Unrecognized file type"))
 
 (comment
-(wav-info "resources/test.wav")
-  (wav-info "resources/saw.wav")
-  )
+  (wav-info "resources/test.wav")
+  (wav-info "resources/saw.wav"))
