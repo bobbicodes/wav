664540e07118a4d4671c21168844f6e1572fa55f
porkostomus <porkostomus@mail.com>
2020-02-25 22:50:39 -0800

    factor out some functions

--- src/porkostomus/wav.clj
+++ src/porkostomus/wav.clj
@@ -1,45 +1,39 @@
 (ns porkostomus.wav
   (:require [clojure.java.io :as io]))
 
-(defn file->bytes [file]
+(defn file->bytes
+  [file]
   (with-open [in  (io/input-stream file)
               out (java.io.ByteArrayOutputStream.)]
     (io/copy in out)
     (.toByteArray out)))
 
-(defn bytes->hex [bytes]
-  (for [b bytes] (format "%02x" b)))
+(defn file->hex
+  [file]
+  (for [byte (file->bytes file)]
+    (format "%02x" byte)))
 
-(defn file->hex [file]
-  (bytes->hex (file->bytes file)))
+(defn ascii-val 
+  [file from to]
+  (apply str (map char (take (- to from)
+                             (drop from (file->bytes file))))))
 
-(defn hex-bytes
-  ([file n] (hex-bytes file n (inc n)))
-  ([file from to]
-   (take (- to from)
-         (drop from (file->hex file)))))
+(defn dec-val 
+  [file from to]
+  (Integer/decode (str "0x" (apply str (reverse (take (- to from)
+                                                      (drop from (file->hex file))))))))
 
-(defn dec-bytes
-  ([file n] (hex-bytes file n (inc n)))
-  ([file from to]
-   (take (- to from)
-         (drop from (file->bytes file)))))
+(defn riff-chunk
+  [file]
+  {:ckID (ascii-val file 0 4)
+   :cksize (dec-val file 4 8)
+   :WAVEID (ascii-val file 8 12)})
 
-(defn ascii-val [bytes]
-  (apply str (map char bytes)))
-
-(defn dec-val [bytes]
-  (Integer/decode (str "0x" (apply str (reverse bytes)))))
-
-(defn riff-chunk [file]
-  {:ckID (ascii-val (dec-bytes file 0 4))
-   :cksize (dec-val (hex-bytes file 4 8))
-   :WAVEID (ascii-val (dec-bytes file 8 12))})
-
-(defn fmt-chunk [file]
-  {:ckID (ascii-val (dec-bytes file 12 16))})
+(defn fmt-chunk
+  [file]
+  {:ckID (ascii-val file 12 16)})
 
 (comment
-  (riff-chunk  "resources/test.wav")
+  (riff-chunk "resources/test.wav")
   (fmt-chunk "resources/test.wav")
   )
